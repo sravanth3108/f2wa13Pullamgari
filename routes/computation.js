9b440f5abbc07aa3f9be0ae379890e5adc8a358e
sravanth3108 <sravanthreddy.pullamgari@gmail.com>
2022-10-26 22:14:08 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -1,16 +1,20 @@
 var express = require('express');
+
 var router = express.Router();
-var rand = Math.floor(Math.random()*100)
-var rand2= Math.random()
-var val1="Math.atan2() applied to "+ rand + " is "+ Math.atan2(rand,rand)
-var val2="Math.atanh()  applied to "+ rand2 + " is "+ Math.atanh(rand2)
-var val3= "Math.cbrt() applied to "+ rand + " is "+ Math.cbrt(rand);
-console.log(val1)
+
+var rand = Math.floor(Math.random()*100);var rand2= Math.random()
+
+var atan2="Math.atan2() applied to "+ rand + " is "+ Math.atan2(rand,rand)
+
+var atanh="Math.atanh()  applied to "+ rand2 + " is "+ Math.atanh(rand2)
+
+var cbrt= "Math.cbrt() applied to "+ rand + " is "+ Math.cbrt(rand);
 
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('computation', { title:'Sravanth Reddy Bonus',value1: val1, value2:val2, value3:val3 });
+  res.render('computation', { title:'Sravanth Reddy Bonus',value1: atan2, value2:atanh, value3:cbrt });
 });
 
 
